08ae7a7d17c77431ff1e7e0b2c4515b681cf93d3
tampler <socnetfpga@gmail.com>
2021-12-05 19:34:02 +0300

    Upd test checkers

--- test/valid_helpers.go
+++ test/valid_helpers.go
@@ -2,42 +2,47 @@ package test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-// validates Terraform output versus expected
-func getOutputs(t *testing.T, opts *terraform.Options) (pubIP, domain, privSubnets, pubSubnets string) {
-	pubIP = terraform.Output(t, opts, "vpc_public_ip")
-	domain = terraform.Output(t, opts, "domain")
-	privSubnets = terraform.Output(t, opts, "subnet_private_id")
-	pubSubnets = terraform.Output(t, opts, "subnet_public_id")
-
-	return
-}
-
 func validatePrivate(t *testing.T, opts *terraform.Options, cfg testCaseT) {
-	pubIP, domain, _, _ := getOutputs(t, opts)
+	pubIP := terraform.Output(t, opts, "vpc_public_ip")
+	domain := terraform.Output(t, opts, "domain")
+	tmpPrivSubnets := terraform.Output(t, opts, "subnet_private_id")
+
+	privSubnets := strings.Fields(trimBrackets(tmpPrivSubnets))
 
 	fmt.Println(">>>>> Pub IP: ", pubIP)
+	fmt.Println(">>>>> Domain: ", domain)
+	fmt.Println(">>>>> Private subnets: ", privSubnets)
 
-	// actName := strings.Fields(trimBrackets(outName))
-	// actID := strings.Fields(trimBrackets(outID))
-
+	assert.NotEmpty(t, pubIP)
 	assert.Equal(t, cfg.domain, domain)
-	// assert.ElementsMatch(t, name, actName)
+	assert.Equal(t, len(cfg.subnetPrivCidr), len(privSubnets))
 }
 
 func validatePublic(t *testing.T, opts *terraform.Options, cfg testCaseT) {
-	pubIP, domain, _, _ := getOutputs(t, opts)
+	pubIP := terraform.Output(t, opts, "vpc_public_ip")
+	domain := terraform.Output(t, opts, "domain")
+	tmpPubSubnets := terraform.Output(t, opts, "subnet_private_id")
+
+	pubSubnets := strings.Fields(trimBrackets(tmpPubSubnets))
 
 	fmt.Println(">>>>> Pub IP: ", pubIP)
+	fmt.Println(">>>>> Domain: ", domain)
+	fmt.Println(">>>>> Private subnets: ", pubSubnets)
 
-	// actName := strings.Fields(trimBrackets(outName))
-	// actID := strings.Fields(trimBrackets(outID))
-
+	assert.NotEmpty(t, pubIP)
 	assert.Equal(t, cfg.domain, domain)
-	// assert.ElementsMatch(t, name, actName)
+	assert.Equal(t, len(cfg.subnetPrivCidr), len(pubSubnets))
+}
+
+func trimBrackets(s string) string {
+	str0 := strings.TrimLeft(s, "[")
+	str1 := strings.TrimRight(str0, "]")
+	return str1
 }
