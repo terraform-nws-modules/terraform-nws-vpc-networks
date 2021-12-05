ff5288e939dfd3fcfdfa564b8d5984ac45fdaaeb
tampler <socnetfpga@gmail.com>
2021-12-05 16:39:47 +0300

    Add initial test

---
+++ test/nws_vpc_network_test.go
@@ -0,0 +1,52 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestVpcPrivateExample(t *testing.T) {
+	t.Parallel()
+
+	stage := test_structure.RunTestStage
+
+	testCases := []testCaseT{
+		{
+			"VPC with private subnetworks",
+			[]string{genName()},
+			[]string{"10.0.1.0/30"},
+			[]string{},
+			[]string{},
+		},
+	}
+	for _, testCase := range testCases {
+
+		// capture range variable so that it doesn't update when the subtest goroutine swaps.
+		testCase := testCase
+
+		// generate a random service path for each parallel test
+		servicePath := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/vpc-private")
+
+		// fork a parallel test with all stages
+		t.Run(testCase.testName, func(t *testing.T) {
+			t.Parallel()
+			stage(t, "deploy", func() {
+				opts := config(t, testCase, servicePath)
+				test_structure.SaveTerraformOptions(t, servicePath, opts)
+				terraform.InitAndApply(t, opts)
+			})
+
+			defer stage(t, "destroy", func() {
+				opts := test_structure.LoadTerraformOptions(t, servicePath)
+				terraform.Destroy(t, opts)
+			})
+
+			stage(t, "validate", func() {
+				opts := test_structure.LoadTerraformOptions(t, servicePath)
+				validate(t, opts)
+			})
+		})
+	}
+}
---
+++ test/test_helpers.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+const (
+	vpcName = "vpc-test"
+	vpcCidr = "10.0.1.0/24"
+	domain  = "test.local"
+)
+
+type testCaseT struct {
+	testName       string
+	subnetPrivName []string
+	subnetPrivCidr []string
+	subnetPubName  []string
+	subnetPubCidr  []string
+}
+
+func config(t *testing.T, cfg testCaseT, servicePath string) *terraform.Options {
+
+	return terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: servicePath,
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"vpc_name":            vpcName,
+			"vpc_cidr":            vpcCidr,
+			"domain":              domain,
+			"subnet_private_name": cfg.subnetPrivName,
+			"subnet_private_cidr": cfg.subnetPrivCidr,
+			"subnet_public_name":  cfg.subnetPubName,
+			"subnet_public_cidr":  cfg.subnetPubCidr,
+		},
+		// Add retries to eliminate trasilent errors
+		MaxRetries:         3,
+		TimeBetweenRetries: 5 * time.Second,
+	})
+}
+
+// validates Terraform output versus expected
+func validate(t *testing.T, opts *terraform.Options) {
+	// outName := terraform.Output(t, opts, "name")
+	// outID := terraform.Output(t, opts, "id")
+
+	// actName := strings.Fields(trimBrackets(outName))
+	// actID := strings.Fields(trimBrackets(outID))
+
+	// // assert.Equal(t, len(name), len(actID))
+	// assert.ElementsMatch(t, name, actName)
+}
+
+// Validation helpers
+func trimBrackets(s string) string {
+	str0 := strings.TrimLeft(s, "[")
+	str1 := strings.TrimRight(str0, "]")
+	return str1
+}
+
+func genName() string {
+	return fmt.Sprintf("vpc-test-%s", random.UniqueId())
+}
